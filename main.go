253a52ca2bfb16c5475e06ae50cfd08a4185daec
Mauro Lavigna <mauro.lavigna@gmail.com>
2022-03-24 09:20:15 -0300

    main: fix the reverse bug found by fuzz

--- main.go
+++ main.go
@@ -1,20 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
 	input := "The quick brown fox jumped over the lazy dog"
-	rev := Reverse(input)
-	doubleRev := Reverse(rev)
+	rev, err := Reverse(input)
+	doubleRev, doubleErr := Reverse(rev)
 	fmt.Printf("original: %q\n", input)
-	fmt.Printf("reversed: %q\n", rev)
-	fmt.Printf("reversed back: %q\n", doubleRev)
+	fmt.Printf("reversed: %q\n, err: %v\n", rev, err)
+	fmt.Printf("reversed back: %q\n, err: %v\n", doubleRev, doubleErr)
 }
 
-func Reverse(s string) string {
+func Reverse(s string) (string, error) {
+	if !utf8.ValidString(s) {
+		return s, errors.New("input is not valid UTF-8")
+	}
 	r := []rune(s)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
 		r[i], r[j] = r[j], r[i]
 	}
-	return string(r)
+	return string(r), nil
 }
